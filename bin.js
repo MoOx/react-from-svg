bb194d2f849dde81926feb2d8316c738ad3ba9d1
Maxime Thirouin <git@moox.io>
2020-01-23 15:20:14 +0100

    format

--- bin.js
+++ bin.js
@@ -26,7 +26,7 @@ if (!outputPath) {
   throw new Error("output path is required");
 }
 if (splitArgs.filter(arg => !acceptedOptions.includes(arg[0])).length > 0) {
-  throw new Error("only accepted options are: "+acceptedOptions);
+  throw new Error("only accepted options are: " + acceptedOptions);
 }
 
 ///
@@ -34,35 +34,40 @@ if (splitArgs.filter(arg => !acceptedOptions.includes(arg[0])).length > 0) {
 let compiledArtifact = "./lib/js/src/Transformer.bs.js";
 let transformer;
 try {
-  transformer = require(compiledArtifact)
-}
-catch(e) {
-  console.log("react-from-svg: Missing compiled "+compiledArtifact+", rebuilding");
-  require("child_process")
-  .execSync("bsb -clean-world -make-world", {
+  transformer = require(compiledArtifact);
+} catch (e) {
+  console.log(
+    "react-from-svg: Missing compiled " + compiledArtifact + ", rebuilding",
+  );
+  require("child_process").execSync("bsb -clean-world -make-world", {
     cwd: __dirname,
     stdio: "inherit",
   });
 }
 
-setTimeout(function () {
+setTimeout(function() {
   try {
     delete require.cache[compiledArtifact];
-    transformer = require(compiledArtifact)
-  }
-  catch(e) {
+    transformer = require(compiledArtifact);
+  } catch (e) {
     throw new Error(
-      "react-from-svg: missing compiled script. You might have used a `bsb -clean-world` which is currently causing trouble to the way this bin is generated."
-      +"\n"+
-      "***************************************************************************************************************"
-      +"\n"+
-      "** Try running this command again, we tried to compile it asynchronously (should be ready as you read this). **"
-      +"\n"+
-      "***************************************************************************************************************"
-      +"\n"+
-      "If this issue persist, open an issue with as much as information as you can (bsconfig, etc) https://github.com/MoOx/react-from-svg/issues/new"
-    )
-  };
+      "react-from-svg: missing compiled script. You might have used a `bsb -clean-world` which is currently causing trouble to the way this bin is generated." +
+        "\n" +
+        "***************************************************************************************************************" +
+        "\n" +
+        "** Try running this command again, we tried to compile it asynchronously (should be ready as you read this). **" +
+        "\n" +
+        "***************************************************************************************************************" +
+        "\n" +
+        "If this issue persist, open an issue with as much as information as you can (bsconfig, etc) https://github.com/MoOx/react-from-svg/issues/new",
+    );
+  }
 
-  transformer.make(sourcePath, outputPath, hasArg(splitArgs, "--with-reason"), hasArg(splitArgs, "--remove-fill"), getArg(splitArgs, "--absolute-path"));
+  transformer.make(
+    sourcePath,
+    outputPath,
+    hasArg(splitArgs, "--with-reason"),
+    hasArg(splitArgs, "--remove-fill"),
+    getArg(splitArgs, "--absolute-path"),
+  );
 }, 0);
